78b4f5f7fffd8319c9903b0b29194549160e18b5
Hakan <Hakan@DESKTOP-13SUEK0.local>
2021-12-06 01:04:22 +0300

    41 - BankAccount class added.

---
+++ Sparky/Sparky/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿namespace Sparky
+{
+    public class BankAccount
+    {
+        public int Balance { get; set; }
+
+        public BankAccount()
+        {
+            Balance = 0;
+        }
+
+        public bool Deposit(int amount)
+        {
+            Balance += amount;
+            return true;
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public int GetBalance()
+        {
+            return Balance;
+        }
+
+    }
+}
