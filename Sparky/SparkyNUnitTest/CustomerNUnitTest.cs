63a5b13dae4a4f722d278da164ca33ba8bc90e72
Hakan <Hakan@DESKTOP-13SUEK0.local>
2021-12-05 20:05:14 +0300

    3.11 - String Assertion helper methods.

--- Sparky/SparkyNUnitTest/CustomerNUnitTest.cs
+++ Sparky/SparkyNUnitTest/CustomerNUnitTest.cs
@@ -17,6 +17,13 @@ namespace SparkyNUnitTest
 
             // Assert
             Assert.That(fullName, Is.EqualTo("Hello, Ben Spark"));
+            Assert.That(fullName, Does.Contain(","));
+            Assert.That(fullName, Does.StartWith("Hello,"));
+            Assert.That(fullName, Does.EndWith("Spark"));
+            Assert.That(fullName, Does.Contain("ben Spark").IgnoreCase); // IgnoreCase -> Ignores case sensitivity.
+
+            // Asserting with regular expressions
+            Assert.That(fullName, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
         }
     }
 }
