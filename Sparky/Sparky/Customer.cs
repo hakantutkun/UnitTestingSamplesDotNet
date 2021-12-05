2dae632fc4fdce9d5ac61ff56aead30abed015d7
Hakan <Hakan@DESKTOP-13SUEK0.local>
2021-12-05 19:52:14 +0300

    3.10 - Asserting string values...

---
+++ Sparky/Sparky/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Sparky
+{
+    public class Customer
+    {
+        public string GreetAndCombineNames(string firstName, string LastName)
+        {
+            return $"Hello, {firstName} {LastName}";
+        }
+    }
+}
---
+++ Sparky/SparkyNUnitTest/CustomerNUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Sparky;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class CustomerNUnitTest
+    {
+        [Test]
+        public void CombineName_InputFirstAndLastName_ReturnFullName()
+        {
+            // Arrange
+            var customer = new Customer();
+
+            // Act
+            string fullName = customer.GreetAndCombineNames("Ben", "Spark");
+
+            // Assert
+            Assert.That(fullName, Is.EqualTo("Hello, Ben Spark"));
+        }
+    }
+}
