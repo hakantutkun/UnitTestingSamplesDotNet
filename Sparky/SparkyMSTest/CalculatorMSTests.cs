fbb2b7bdb0db3d9a2c3d695cd40ca962e9f62518
Hakan <Hakan@DESKTOP-13SUEK0.local>
2021-12-05 17:22:24 +0300

    2.3 - First test added.

---
+++ Sparky/SparkyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sparky;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            // Arrange -> All the initializations should be arranged this phase.
+            // Create an object of the class that will be tested.
+            Calculator calc = new();
+
+            // Act -> The phase that actually should invoke any methods that are needed that will give us some output.
+            int result = calc.AddNumbers(10,20);
+
+            // Assert -> The phase that we should actually check that the result is exactly the same as we expected.
+            Assert.AreEqual(30, result);
+        }
+    }
+}
+   
