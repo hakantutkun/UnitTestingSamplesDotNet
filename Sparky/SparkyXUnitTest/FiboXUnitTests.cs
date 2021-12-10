2bfc0f30b1a25950ddc6bfa4c712fda4fdfeb0db
Hakan <Hakan@DESKTOP-13SUEK0.local>
2021-12-10 23:07:00 +0300

    65 - Fibo XUnit tests rearranged.

--- Sparky/SparkyXUnitTest/FiboXUnitTests.cs
+++ Sparky/SparkyXUnitTest/FiboXUnitTests.cs
@@ -1,66 +1,64 @@
-﻿//using NUnit.Framework;
-//using Sparky;
+﻿using Sparky;
+using Xunit;
 
-//namespace SparkyXUnitTest
-//{
-//    /// <summary>
-//    /// A test class for testing Fibo class methods
-//    /// </summary>
-//    [TestFixture]
-//    internal class FiboXUnitTests
-//    {
-//        /// <summary>
-//        /// Fibo Object
-//        /// </summary>
-//        private Fibo _fibo;
+namespace SparkyXUnitTest
+{
+    /// <summary>
+    /// A test class for testing Fibo class methods
+    /// </summary>
+    public class FiboXUnitTests
+    {
+        /// <summary>
+        /// Fibo Object
+        /// </summary>
+        private Fibo _fibo;
 
-//        /// <summary>
-//        /// Setting up the test environment
-//        /// </summary>
-//        [SetUp] 
-//        public void SetUpAttribute()
-//        {
-//            // Create an instance of fibo
-//            _fibo = new Fibo();
-//        }
+        /// <summary>
+        /// Setting up the test environment
+        /// </summary>
+        public FiboXUnitTests()
+        {
+            // Create an instance of fibo
+            _fibo = new Fibo();
+        }
 
-//        /// <summary>
-//        /// Test with range = 1
-//        /// </summary>
-//        [Test]
-//        public void GetFiboSeries_InputRange1_ReturnsFiboSeries()
-//        {
-//            // Arrange
-//            _fibo.Range = 1;
-//            List<int> expectedRange = new List<int>() { 0 };
+        /// <summary>
+        /// Test with range = 1
+        /// </summary>
+        [Fact]
+        public void GetFiboSeries_InputRange1_ReturnsFiboSeries()
+        {
+            // Arrange
+            _fibo.Range = 1;
+            List<int> expectedRange = new List<int>() { 0 };
 
-//            // Act
-//            var result = _fibo.GetFiboSeries();
+            // Act
+            var result = _fibo.GetFiboSeries();
 
-//            // Assert
-//            Assert.That(result, Is.Not.Empty);
-//            Assert.That(result, Is.Ordered);
-//            Assert.That(result, Is.EquivalentTo(expectedRange));
-//        }
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Equal(expectedRange.OrderBy(u => u), result);
+            Assert.True(result.SequenceEqual(expectedRange));
+        }
 
-//        /// <summary>
-//        /// Test with range = 6
-//        /// </summary>
-//        [Test]
-//        public void GetFiboSeries_InputRange6_ReturnsFiboSeries()
-//        {
-//            // Arrange
-//            _fibo.Range = 6;
-//            List<int> expectedRange = new List<int>() { 0,1,1,2,3,5 };
+        /// <summary>
+        /// Test with range = 6
+        /// </summary>
+        [Fact]
+        public void GetFiboSeries_InputRange6_ReturnsFiboSeries()
+        {
+            // Arrange
+            _fibo.Range = 6;
+            List<int> expectedRange = new List<int>() { 0, 1, 1, 2, 3, 5 };
 
-//            // Act
-//            var result = _fibo.GetFiboSeries();
+            // Act
+            var result = _fibo.GetFiboSeries();
 
-//            // Assert
-//            Assert.That(result, Does.Contain(3));
-//            Assert.That(result.Count,Is.EqualTo(6));
-//            Assert.That(result, Has.No.Member(4));
-//            Assert.That(result, Is.EquivalentTo(expectedRange));
-//        }
-//    }
-//}
+            // Assert
+            Assert.Contains(3, result);
+            Assert.Equal(6,result.Count);
+            Assert.DoesNotContain(4, result);
+            Assert.Equal(expectedRange, result);
+        }
+    }
+}
