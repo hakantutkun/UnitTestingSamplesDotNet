7f445e3bf66a2a8def4f520b0eebcbf34a9fa2e6
Hakan <Hakan@DESKTOP-13SUEK0.local>
2021-12-12 16:15:25 +0300

    85 - Bongo Web Index Action Method Unit Test

---
+++ Sparky/Bongo.Web.Tests/RoomBookingControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Bongo.Core.Services.IServices;
+using Bongo.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace Bongo.Web.Tests
+{
+    [TestFixture]
+    public class RoomBookingControllerTests
+    {
+        private Mock<IStudyRoomBookingService> _studyRoomBookingService;
+        private RoomBookingController _bookingController;
+
+        [SetUp] 
+        public void SetUp()
+        {
+            _studyRoomBookingService = new Mock<IStudyRoomBookingService>();
+            _bookingController = new RoomBookingController(_studyRoomBookingService.Object);
+        }
+
+        [Test]
+        public void IndexPage_CallRequest_VerifyAllInvoked()
+        {
+            _bookingController.Index();
+            _studyRoomBookingService.Verify(x=> x.GetAllBooking(), Times.Once);
+        }
+    }
+}
